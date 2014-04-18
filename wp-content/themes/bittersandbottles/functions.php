a75e258c7e13c8396be5d022afce2ed368aebdc2
Dennis Yang <sinned@gmail.com>
2014-04-18 15:51:24 -0700

    adding cut images

--- wp-content/themes/bittersandbottles/functions.php
+++ wp-content/themes/bittersandbottles/functions.php
@@ -9,4 +9,12 @@ function foxyshop_custom_css() {
 //--------------------------------------------------------------------------------------------------------------
 
 
-
+// add special class to the Home Navigation
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+function special_nav_class($classes, $item){
+     if($item->title == "Home"){ //Notice you can change the conditional from is_single() and $item->title
+             $classes[] = "home-nav-class";
+     } else {
+     }
+     return $classes;
+}
