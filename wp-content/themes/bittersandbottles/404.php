feaee4671755cf07733c93f652d944e36d76a6e0
Dennis Yang <sinned@gmail.com>
2014-10-25 01:55:43 -0700

    adding 404 page.

---
+++ wp-content/themes/bittersandbottles/404.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+
+		<div class="primary page-header">
+			<div class="main">				
+				<h3><span class="super-plus">Page not found</span></h3>
+			</div>
+		</div>
+
+		<div id="primary">
+			<div id="content" role="main" class="pagecontent indentedlists">
+				<div class="cocktails-page" style="padding:100px 0;">
+					Sorry. You've reached a page that does not exist.
+					<br /><br />
+					Perhaps you were looking for the <a href="http://shop.bittersandbottles.com/">shop</a>?
+
+				</content>
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+
+
+
+<?php get_footer(); ?>
