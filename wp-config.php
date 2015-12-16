b93d16337281a3c48d7bd16b3f1da52d1044758d
Dennis Yang <sinned@gmail.com>
2015-12-16 12:47:20 -0800

    removing super cache

--- wp-config.php
+++ wp-config.php
@@ -103,8 +103,6 @@ define('WP_DEBUG', false);
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
-define( 'WP_CACHE', true );
-
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
 
